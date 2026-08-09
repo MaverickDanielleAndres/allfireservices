469029d6b213ac8ea09076f22e8e2a073ebd154f
MaverickDanielleAndres <maverickdanielle@gmail.com>
2026-08-09 03:49:33 -0700

    update images

--- components/HomeStoryLegacy.tsx
+++ components/HomeStoryLegacy.tsx
@@ -38,9 +38,9 @@ const generationReveal: Variants = {
 };
 
 const generations = [
-  { year: "1911", relation: "Uncle", name: "William Tricklebank", image: "/family/greatgrandad.jpg" },
-  { year: "1931", relation: "Granddad", name: "Trevor Tricklebank", image: "/family/grandfathertrevor.jpg" },
-  { year: "1955", relation: "Uncle", name: "Trevor Tricklebank Jr", image: "/family/uncletrevortricklebandjr.jpg" },
+  { year: "1911", relation: "Uncle", name: "William Tricklebank", image: "/family/unclewilliam.PNG" },
+  { year: "1931", relation: "Granddad", name: "Trevor Tricklebank", image: "/family/waltergreatgrandad.png" },
+  { year: "1955", relation: "Uncle", name: "Trevor Tricklebank Jr", image: "/family/uncletrevortricklebandjr.PNG" },
   { year: "1957", relation: "My Father", name: "Stanley Tricklebank", image: "/family/Myfather.png" },
   { year: "1959", relation: "Uncle", name: "Ian Tricklebank", image: "/family/uncleian.png" },
   { year: "Current", relation: "Current", name: "Peter Tricklebank", image: "/family/pete.png" },
